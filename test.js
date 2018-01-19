7602a426cb4002dc75fcfdd8076da5e684017358
Mark Arciaga <marciaga@flightstats.com>
2018-01-19 15:49:39 -0800

    Handle case where we want to report an undefined value. Add test case.

--- test.js
+++ test.js
@@ -11,6 +11,16 @@ e.data = {
 console.error('console.error', 'There was a problem.', e);
 console.log('console.log', 'There was a problem.', e);
 
-const o = { b: true, s: 'string', i: 10, f: 56.65, a: ['a', 'b', 'c'], o: { o2: 'object 2' } };
+const o = {
+  b: true,
+  s: 'string',
+  i: 10,
+  f: 56.65,
+  a: ['a', 'b', 'c'],
+  o: { o2: 'object 2' },
+  u: undefined,
+  n: null,
+};
+
 o.circ = o;
 console.log('JSON error logging enabled', o);
